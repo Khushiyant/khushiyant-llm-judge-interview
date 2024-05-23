397ebd6b9d76c0f70b842ed73b9e4b3973437026
Khushiyant <khushiyant2002@gmail.com>
2024-05-23 18:17:29 +0530

    chore: Update question dump script

--- scripts/dump_martian_db.py
+++ scripts/dump_martian_db.py
@@ -1,76 +1,50 @@
-"""
-This script is setup to dump the martian database transactions to disk.
-
-This dumps to pickle as ObjectID is not serializable to JSON.
-
-"""
-
 import os
-
 import pymongo
 import pickle
 from jsonargparse import ActionConfigFile, ArgumentParser
 from dotenv import load_dotenv
 from llm_judge.utils.logger import info_logger as logging
 
+"""
+This script is setup to dump the martian database transactions to disk. 
+
+This dumps to pickle as ObjectID is not serializable to JSON. 
+
+"""
+
 load_dotenv("./.env", override=True)
 
 
-def get_users_by_email(email: str, connection_string: str) -> list:
+def get_prompts(connection_string: str) -> list:
     client = pymongo.MongoClient(connection_string)
-    db = client["backend"]["users"]
-    results = []
-    results += list(db.find({"email": {"$regex": email}}))
-    return results
+    db = client["llm-judge"]["prompts"]
+    prompts = list(db.find())
+    return prompts
 
 
-def get_members_by_user_id(user_ids: list, connection_string: str) -> list:
+def get_questions(connection_string: str) -> list:
     client = pymongo.MongoClient(connection_string)
-    member_db = client["backend"]["members"]
-    members = list(member_db.find({"user": {"$in": user_ids}}))
-    return members
+    db = client["llm-judge"]["questions"]
+    questions = list(db.find())
+    return questions
 
 
-def get_orgs_by_member_id(member_ids: list, connection_string: str) -> list:
-    client = pymongo.MongoClient(connection_string)
-    org_db = client["backend"]["organizations"]
-    orgs = list(org_db.find({"members": {"$in": member_ids}}))
-    return orgs
+def main(connection_string: str, output_file: str, prompt_file: str) -> None:
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-
-def get_transactions_by_org_id(
-    org_ids: list, limit: int, connection_string: str
-) -> list:
-    client = pymongo.MongoClient(connection_string)
-    db = client["backend"]["transactions_v2"]
-    results = []
-    results += list(db.find({"org_local_id": {"$in": org_ids}}).limit(limit))
-    return results
-
-
-def main(
-    email_regex: str, connection_string: str, limit: int, output_file: str
-) -> None:
-    users = get_users_by_email(email_regex, connection_string)
-    user_ids = [user["_id"] for user in users]
-    members = get_members_by_user_id(user_ids, connection_string)
-    member_ids = [member["_id"] for member in members]
-    orgs = get_orgs_by_member_id(member_ids, connection_string)
-    org_ids = [org["local_id"] for org in orgs]
-    transactions = get_transactions_by_org_id(org_ids, limit, connection_string)
-    logging.info(f"Found {len(transactions)} transactions")
+    questions = get_questions(connection_string)
+    logging.info(f"Dumping {len(questions)} questions to {output_file}")
     with open(output_file, "wb") as f:
-        pickle.dump(transactions, f)
+        pickle.dump(questions, f)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--email_regex", type=str)
-    parser.add_argument("--limit", type=int, default=1000000)
     parser.add_argument(
         "--connection_string", type=str, default=os.environ["MARTIAN_MONGO_URI"]
     )
-    parser.add_argument("--output_file", type=str, default="transaction_dump.pkl")
+    parser.add_argument("--output_file", type=str, default="question_dump.pkl")
+    parser.add_argument("--prompt_file", type=str, default="prompt_dump.pkl")
     parser.add_argument("--config", action=ActionConfigFile)
     args = parser.parse_args()
-    main(args.email_regex, args.connection_string, args.limit, args.output_file)
+    main(args.connection_string, args.output_file, args.prompt_file)
